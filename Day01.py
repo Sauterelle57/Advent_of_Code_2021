29bc70734a11d843acb37aeb11096ca10bd98045
Marie Giacomel <marie.giacomel@epitech.eu>
2021-12-04 11:38:11 +0100

    Day01 part 2

--- Day01.py
+++ Day01.py
@@ -5,11 +5,25 @@
 ## Day01
 ##
 
+increase = 0
 
-with open("input.txt", "r") as file:
-    data = file.readline()
-    while data != "":
-        print(data)
-        data = file.readline()
+with open("/home/Marie/Advent_of_Code_2021/input.txt", "r") as file:
+    data1 = int(file.readline())
+    data2 = int(file.readline())
+    data3 = int(file.readline())
+    somme = data1 + data2 + data3
+    while data3 != "":
+        print("s:", somme, "S:", data1 + data2 + data3)
+        if somme < data1 + data2 + data3:
+            increase += 1
+        somme = data1 + data2 + data3
+        data1 = data2
+        data2 = data3
+        data3 = file.readline()
+        if data3 != "":
+            data3 = int(data3)
+        else:
+            break
 
+print(increase)
 file.close()
