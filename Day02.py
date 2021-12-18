cda5ed4409af28abb81cfd3666d22cf945996aca
Marie Giacomel <marie.giacomel@epitech.eu>
2021-12-18 16:39:05 +0100

    advent of code 2021

--- Day02.py
+++ Day02.py
@@ -6,16 +6,18 @@
 ##
 
 pos = [0, 0] #horizontal pos, depth
+aim = 0
 
 with open("/home/Marie/Advent_of_Code_2021/input_day02.txt", "r") as file:
     new_pos = file.readline()
     while new_pos != "":
         if "forward" in new_pos:
             pos[0] += int(new_pos[-2])
+            pos[1] += int(new_pos[-2]) * aim
         if "down" in new_pos:
-            pos[1] += int(new_pos[-2])
+            aim += int(new_pos[-2])
         if "up" in new_pos:
-            pos[1] -= int(new_pos[-2])
+            aim -= int(new_pos[-2])
         new_pos = file.readline()
 
 print(pos[0] * pos[1])
